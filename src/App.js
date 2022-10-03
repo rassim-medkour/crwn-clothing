af27ef16613b9e828d40d80ff0c5fca61ab09bf8
Rassim Medkour <racimmedkour@gmail.com>
2022-10-03 10:20:44 +0100

    added routes for homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
+import {Routes,Route} from 'react-router-dom';
 import './App.css';
 import {HomePage} from './Pages/homepage/homepage.js';
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <Routes>
+        <Route exact path='/' element={<HomePage/>} ></Route>
+      </Routes>
     </div>
   );
 }
